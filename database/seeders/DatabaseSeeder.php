4d6156bcdb8f774705b430a3fc540525ba30c9cb
Mahmudul Haque <mahmudul21124@gmail.com>
2025-03-03 03:37:29 +0600

    Step 3: Role Based Access Control(RBAC)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +22,22 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        $admin = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin2@gmail.com',
+            'password' => bcrypt('admin123'),
+        ]);
+
+        $admin->assignRole('Admin');
+
+        $user = User::create([
+            'name' => 'Regular User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user123'),
+        ]);
+
+        $user->assignRole('User');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::middleware(['auth', 'role:Admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
